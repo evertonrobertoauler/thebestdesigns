c2ff60f67fb2dbf86b924ce6581e081f72197d31
Everton R. Auler <evertonrobertoauler@gmail.com>
2017-06-05 16:30:13 -0300

    Adding gravitate design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ScrollClassDirective } from './ui/scroll-class.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrollClassDirective
   ],
   imports: [
     BrowserModule
